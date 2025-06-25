336824d44d61c14653c0c39ce09da62419b789dc
Pratyushaa94 <prathyusha9449@gmail.com>
2025-06-25 21:12:32 +0530

    change in variable

--- mian.tf
+++ mian.tf
@@ -1,13 +1,18 @@
+resource "azurerm_resource_group" "psql_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 resource "azurerm_postgresql_flexible_server" "psql" {
   name                   = var.psql_server_name
-  resource_group_name    = var.resource_group_name
-  location               = var.location
+  resource_group_name    = azurerm_resource_group.psql_rg.name
+  location               = azurerm_resource_group.psql_rg.location
   administrator_login    = var.psql_admin_username
   administrator_password = var.psql_admin_password
   version                = "13"
 
   storage_mb             = 32768
-  sku_name               = "B_Standard_B1ms"  #  Valid low-cost burstable SKU
+  sku_name               = "B_Standard_B1ms"
   zone                   = "1"
 
   backup_retention_days        = 7
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,4 @@ psql_server_name      = "prathyusha-psql-server"
 resource_group_name   = "psql-rg"
 location              = "East US"
 psql_admin_username   = "psqladmin"
+# Note: Do NOT include password here
